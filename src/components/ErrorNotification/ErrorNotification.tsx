fe54a3e2228334a47af8097c40220025f7d5ff5c
vvvladislovvve <vslav13613@gmail.com>
2025-01-18 19:52:01 +0500

    add media-query

--- src/components/ErrorNotification/ErrorNotification.tsx
+++ src/components/ErrorNotification/ErrorNotification.tsx
@@ -4,7 +4,7 @@ export const ErrorNotification = () => {
 
   return (
     <div className={styles.errorNotification}>
-        <img className={styles.errorImg} src="../public/error_dog_vk.jpg" alt="картинка ошибки" />
+        <img className={styles.errorImg} src="error_dog_vk.jpg" alt="картинка ошибки" />
         <h1>Упс! Ошибка!</h1>
         <h2>Кажется, превышен лимит запросов...</h2>
     </div>
